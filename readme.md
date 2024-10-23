860fbee31b91044b41f859000ae25ee25aa01121
jporozco_scging <jporozco@sistecredito.com>
2024-10-23 15:58:45 -0500

    Ajuste funcionamiento del  reto 1 y creacion del  readme

---
+++ readme.md
@@ -0,0 +1,26 @@
+# Topicos Avanzados Reto 1 y Reto 2 Semana 3
+Juan Pablo Vanegas
+Juan Pablo Orozco
+Camilo Echeverri
+
+## Descripción
+Esta es una aplicación de pruebas desarrollada para la entrega 3 del curso de Tópicos Avanzados. Su propósito es demostrar el uso de diversas tecnologías y prácticas avanzadas en el desarrollo de software.
+
+## Características
+- **Pruebas Unitarias**: Implementación de pruebas unitarias para asegurar la calidad del código.
+## Requisitos
+- **Lenguaje**:Java
+- **Framework**: IntelliJ IDEA Comunity
+
+## Instalación
+1. Clonar el repositorio:
+    ```bash
+    git clone [URL del repositorio]
+    ```
+2. Navegar al directorio del proyecto:
+    ```bash
+    cd entrega3topicosavanzados
+    ```
+3. Instalar Dependencias:
+
+   Plugin Loombok
--- src/test/java/com/ensayo3/Reto1Tests.java
+++ src/test/java/com/ensayo3/Reto1Tests.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.time.Duration;
 import java.util.Random;
@@ -21,11 +22,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
-public class Reto1Tests {
-
+class Reto1Tests {
     private static final String BASE_URI = "https://demoqa.com/";
     private static final String PASSWORD = "StringTest0!";
-
     private WebDriver driver;
 
     @BeforeEach
@@ -39,90 +38,60 @@ public class Reto1Tests {
         StringBuilder username = new StringBuilder();
         Random random = new Random();
         int length = 10;
-
         for (int i = 0; i < length; i++) {
             int index = random.nextInt(characters.length());
             username.append(characters.charAt(index));
         }
-
         return "user_" + username;
     }
 
     private RegisterUserResponse createUser(String registerUserRequest) {
         String uri = "Account/v1/User";
+        return given().contentType(ContentType.JSON).accept(ContentType.JSON).body(registerUserRequest).when().post(BASE_URI + uri).then().statusCode(201).extract().as(RegisterUserResponse.class);
+    }
 
-        return given()
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .body(registerUserRequest)
-                .when()
-                .post(BASE_URI + uri)
-                .then()
-                .statusCode(201)
-                .extract().as(RegisterUserResponse.class);
+    private void loginUser(String userName) {
+        driver.get("https://demoqa.com/login");
+        WebElement userNameField = driver.findElement(By.xpath("//label[text()='UserName : ']/following::input[1]"));
+        WebElement passwordField = driver.findElement(By.xpath("//label[text()='Password : ']/following::input[1]"));
+        userNameField.sendKeys(userName);
+        passwordField.sendKeys(PASSWORD);
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Login']"));
+        loginButton.click();
     }
 
     @Test
     public void registerUser() {
         String userName = generateRandomUsername();
-        String registerUserRequestJson = "{\n" +
-                "    \"userName\": \"" + userName + "\",\n" +
-                "    \"password\": \"" + PASSWORD + "\"\n" +
-                "}";
+        String registerUserRequestJson = "{\n" + "    \"userName\": \"" + userName + "\",\n" + "    \"password\": \"" + PASSWORD + "\"\n" + "}";
         RegisterUserResponse response = createUser(registerUserRequestJson);
-
         assertNotNull(response);
         assertEquals(response.getUsername(), userName);
         assertNotNull(response.getUserID());
         log.info("User {} validated for userID: {}", userName, response.getUserID());
-
         log.info("### Logining with user {}", userName);
-        driver.get("https://demoqa.com/login");
-
-        WebElement userNameField = driver.findElement(By
-                .xpath("//label[text()='UserName : ']/following::input[1]"));
-        WebElement passwordField = driver.findElement(By
-                .xpath("//label[text()='Password : ']/following::input[1]"));
-        userNameField.sendKeys(userName);
-        passwordField.sendKeys(PASSWORD);
-
-        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Login']"));
-        loginButton.click();
-
+        loginUser(userName);
         log.info("User logged successfully!... Removing account!");
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        WebElement deleteAccountButton = wait.until(ExpectedConditions
-                .visibilityOfElementLocated(By
-                        .xpath("//button[@id='submit' and text()='Delete Account'] ")));
-
+        WebElement deleteAccountButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='submit' and text()='Delete Account'] ")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", deleteAccountButton);
         deleteAccountButton.click();
         WebElement okButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("closeSmallModal-ok")));
         okButton.click();
         log.info("Account {} removed successfully!", userName);
-
         wait.until(ExpectedConditions.alertIsPresent());
         Alert alert = driver.switchTo().alert();
         alert.accept();
-
         log.info("Trying to login again with removed user credentials...");
-
-        userNameField = driver.findElement(By
-                .xpath("//label[text()='UserName : ']/following::input[1]"));
-        passwordField = driver.findElement(By
-                .xpath("//label[text()='Password : ']/following::input[1]"));
-
+        WebElement userNameField = driver.findElement(By.xpath("//label[text()='UserName : ']/following::input[1]"));
+        WebElement passwordField = driver.findElement(By.xpath("//label[text()='Password : ']/following::input[1]"));
         userNameField.sendKeys(userName);
         passwordField.sendKeys(PASSWORD);
-        loginButton = driver.findElement(By.xpath("//button[text()='Login']"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Login']"));
         loginButton.click();
-        WebElement errorElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By
-                .xpath("//p[@id='name' and text()='Invalid username or password!']")));
-
+        WebElement errorElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@id='name' and text()='Invalid username or password!']")));
         assertEquals("Invalid username or password!", errorElement.getText());
-
         log.info("Error message validated successfully!");
         driver.close();
     }
-
 }
